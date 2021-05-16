61c0e65eded84c9af914f05c427ec2f4e67b38eb
Ashutosh Chaudhary <216.ashutosh@gmail.com>
2021-05-16 12:06:43 +0200

    init

--- brew.sh
+++ brew.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 
-# Install command-line tools using Homebrew.
+###############################################################################
+# My Customizations                                                       #
+###############################################################################
+echo "Hello $(whoami)! Let's get you set up."
+read -p "Enter your emailid: " my_email
 
+echo "mkdir -p ${HOME}/code"
+mkdir -p "${HOME}/code"
+
+echo "installing homebrew"
+# install homebrew https://brew.sh
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo "brew installing stuff"
+
+# Install command-line tools using Homebrew.
 # Make sure we’re using the latest Homebrew.
 brew update
 
@@ -13,89 +27,64 @@ BREW_PREFIX=$(brew --prefix)
 
 # Install GNU core utilities (those that come with macOS are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils
+brew install git coreutils ripgrep zsh
 ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
 
 # Install some other useful utilities like `sponge`.
-brew install moreutils
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-brew install findutils
+brew install moreutils findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
 brew install gnu-sed --with-default-names
-# Install a modern version of Bash.
-brew install bash
-brew install bash-completion2
-
-# Switch to using brew-installed bash as default shell
-if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
-  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
-  chsh -s "${BREW_PREFIX}/bin/bash";
-fi;
 
 # Install `wget` with IRI support.
 brew install wget --with-iri
-
-# Install GnuPG to enable PGP-signing commits.
-brew install gnupg
-
-# Install more recent versions of some macOS tools.
 brew install vim --with-override-system-vi
-brew install grep
-brew install openssh
-brew install screen
-brew install php
-brew install gmp
+brew install openssh screen gnupg
 
 # Install font tools.
 brew tap bramstein/webfonttools
-brew install sfnt2woff
-brew install sfnt2woff-zopfli
-brew install woff2
-
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
+brew install sfnt2woff sfnt2woff-zopfli woff2
 
 # Install other useful binaries.
-brew install ack
-#brew install exiv2
-brew install git
-brew install git-lfs
-brew install gs
-brew install imagemagick --with-webp
-brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
-brew install rename
-brew install rlwrap
-brew install ssh-copy-id
-brew install tree
-brew install vbindiff
-brew install zopfli
+
+brew install git ripgrep imagemagick p7zip pigz ssh-copy-id \
+tree bat rlwrap rclone
+
+echo "installing oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "installing zsh plugins"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+
+echo "installing node (via n-install)"
+curl -L https://git.io/n-install | bash
+
+echo "node --version: $(node --version)"
+echo "npm --version: $(npm --version)"
+
+echo "installing a few global npm packages"
+npm install --global serve fkill-cli npm-quick-run \
+semantic-release-cli npm-check-updates
+
+echo "installing apps with brew cask"
+brew install --cask brave-browser visual-studio-code \
+qlcolorcode qlmarkdown qlstephen quicklook-json webpquicklook \
+qlvideo spotify qmoji slack
+
+echo "Generating a new SSH key for GitHub"
+ssh-keygen -t rsa -b 4096 -C "$my_email" -f ~/.ssh/id_rsa
+eval "$(ssh-agent -s)"
+touch ~/.ssh/config
+echo "Host *\n AddKeysToAgent yes\n UseKeychain yes\n IdentityFile ~/.ssh/id_rsa" | tee ~/.ssh/config
+ssh-add -K ~/.ssh/id_rsa
+echo "run 'pbcopy < ~/.ssh/id_rsa.pub' and paste that into GitHub"
+
+# get bat and delta all configured
+mkdir -p "${HOME}/.config/bat/themes"
+git clone https://github.com/batpigandme/night-owlish "${HOME}/.config/bat/themes/night-owlish"
+bat cache --build
+
+echo "making system modifications:"
 
 # Remove outdated versions from the cellar.
 brew cleanup
---
+++ synccloud.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+####
+# This script sync private configs from remote cloud to local filesystem
+####
+FOLDERS=(.ssh .aws)
+
+connect_cloud() {
+    DESTN="$1"
+    echo "Generating drive config..."
+    read -p "Enter drive emailid: " cloud_email
+    echo "Please wait while I download secret files...."
+    rclone config create mycloud drive user $cloud_email config_is_local false
+    for folder in ${FOLDERS[@]}; do
+        if [ ! -d "$DESTN/$folder" ]; then
+            mkdir -p "$DESTN/$folder"
+        fi;
+        rclone copy mycloud:/dotfiles/$folder $DESTN/$folder
+    done;
+}
+
+connect_cloud ~
